838ed55152b1dde7a9aafc69a37372c3ce8df120
t-dlsantos <thiagodlsantos9@gmail.com>
2024-10-04 19:12:20 -0300

    refactor: make merge sort solution more readable

--- mergeSort.js
+++ mergeSort.js
@@ -1,41 +1,32 @@
-function mergeSort(array) {
-    let high = array.length;
-    let mid = Math.ceil(high / 2);
+const merge = (arr1, arr2) => {
+    let i = 0;
+    let j = 0;
+    let arrayMerged = [];
 
-    if (high === 1) {
+    while (i < arr1.length && j < arr2.length) {
+        if (arr1[i] < arr2[j]) 
+            arrayMerged.push(arr1[i++]);
+        else
+            arrayMerged.push(arr2[j++]);
+    }
+
+    while(i < arr1.length) 
+        arrayMerged.push(arr1[i++]);
+    while(j < arr2.length)
+        arrayMerged.push(arr2[j++])
+
+    return arrayMerged;
+}
+
+const mergeSort = (array) => {
+    let arraySize = array.length;
+    let mid = Math.ceil(arraySize / 2);
+
+    if (arraySize === 1) {
         return [array[0]];
     }
 
-    const merge = (arr1, arr2) => {
-        let i = 0;
-        let j = 0;
-        let k = 0;
-
-        let arrayMerged = [];
-
-        while (i < arr1.length && j < arr2.length) {
-
-            if (arr1[i] < arr2[j]) {
-                arrayMerged[k++] = arr1[i++];
-            } else if (arr2[j] < arr1[i]) {
-                arrayMerged[k++] = arr2[j++];
-            } else if (arr1[i] == arr2[j]) {
-                arrayMerged[k++] = arr1[i++];
-                arrayMerged[k++] = arr2[j++];
-            }
-        }
-        for(; i < arr1.length; i++) {
-            arrayMerged[k++] = arr1[i];
-        }
-
-        for(; j < arr2.length; j++) {
-            arrayMerged[k++] = arr2[j];
-        }
-
-        return arrayMerged;
-    }
-
-    return merge(mergeSort(array.slice(0,mid)), mergeSort(array.slice(mid,high)))
+    return merge(mergeSort(array.slice(0,mid)), mergeSort(array.slice(mid)))
 }
 
 console.log(mergeSort([3, 2, 1, 13, 8, 5, 0, 1]))
